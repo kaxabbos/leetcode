375ec37046f18ded1acc4e0067f823449b65ed8b
kaxabbos <abbos.kaxramonov@gmail.com>
2024-01-29 10:40:54 +0300

    updated 2024.01.29

--- src/main/java/com/leetcode/easy/E26_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/leetcode/easy/E26_RemoveDuplicatesFromSortedArray.java
@@ -1,18 +1,24 @@
 package main.java.com.leetcode.easy;
 
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class E26_RemoveDuplicatesFromSortedArray {
     static public int removeDuplicates(int[] nums) {
-        HashSet<Integer> integers = new HashSet<>();
+        SortedSet<Integer> integers = new TreeSet<>();
         for (int num : nums) integers.add(num);
-        int index = integers.size();
-
-        return index;
+        int index = 0;
+        for (int i : integers) {
+            nums[index] = i;
+            index++;
+        }
+        return integers.size();
     }
 
     public static void main(String[] args) {
         System.out.println(removeDuplicates(new int[]{1, 1, 2}));
         System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
+        System.out.println(removeDuplicates(new int[]{-3,-1,0,0,0,3,3}));
     }
 }
