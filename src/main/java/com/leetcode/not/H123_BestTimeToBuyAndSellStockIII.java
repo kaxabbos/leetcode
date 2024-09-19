c6d8215be0df443c68b7ba99e1f1468d45ed82ee
kaxabbos <abbos.kaxramonov@gmail.com>
2024-09-19 09:54:41 +0300

    updated 2024.09.19

---
+++ src/main/java/com/leetcode/not/H123_BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,17 @@
+package main.java.com.leetcode.not;
+
+public class H123_BestTimeToBuyAndSellStockIII {
+    static public int maxProfit(int[] prices) {
+        int res = 0;
+
+
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
+        System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
